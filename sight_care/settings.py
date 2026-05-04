a4dd2d39ad16c84c18295561e4a8155e5c7c9884
Dani Bhatti <danibhatti1275@gmail.com>
2026-05-04 14:22:50 -0700

    fix css static files

--- sight_care/settings.py
+++ sight_care/settings.py
@@ -20,9 +20,11 @@ INSTALLED_APPS = [
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
